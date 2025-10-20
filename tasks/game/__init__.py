dbc5f785df665c9a3123d4c87da152bbf70fe92d
R <147962255+Eplice@users.noreply.github.com>
2025-10-20 14:09:26 +0800

    Update __init__.py

--- tasks/game/__init__.py
+++ tasks/game/__init__.py
@@ -52,7 +52,7 @@ def start_game():
         # 适配B服，需要点击“登录”
         auto.click_element("./assets/images/screen/bilibili_login.png", "image", 0.9, take_screenshot=False)
         # 检测账号列表界面，点击“登录其他账号”以进入账号密码输入页
-        auto.click_element("./assets/images/screen/account_log_in.png", "image", 0.9, take_screenshot=False)
+        auto.click_element("./assets/images/screen/login_account_btn.png", "image", 0.9, take_screenshot=False)
         # 适配用户协议和隐私政策更新提示，需要点击“同意”
         auto.click_element("./assets/images/screen/agree_update.png", "image", 0.9, take_screenshot=False)
         # 登录过期
